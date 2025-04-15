fb23624fff588e38b8f4f48aeb6109f25154d1c9
Mahesh Menon <menontmahesh@gmail.com>
2025-04-14 21:24:33 -0400

    adding test images

--- webcam/test/test_blob.py
+++ webcam/test/test_blob.py
@@ -1,18 +1,21 @@
-from ..Webcam import Webcam
+import sys
 import os
+import mediapipe as mp
 
-folder_path = "test/input_images"  # change this to your folder path
+# Add the parent directory to the sys.path so you can import Webcam
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from Webcam import Webcam
+folder_path = "webcam/test/input_images"  # change this to your folder path
 
 a = Webcam()
+PoseLandmarkerResult = mp.tasks.vision.PoseLandmarkerResult
 
 for filename in os.listdir(folder_path):
     if filename.endswith(".png"):
+        print(f"Processing {filename}...")
         file_path = os.path.join(folder_path, filename)
         # Do something with the image
-        with Image.open(file_path) as img:
-            a._process_image(folder_path)
-            print(f"Processing {filename}...")
-            # Example: convert to grayscale
-            grayscale = img.convert("L")
-            # Save or process as needed
-            # grayscale.save(os.path.join(folder_path, f"gray_{filename}"))
+        img = mp.Image.create_from_file(file_path)
+        result = PoseLandmarkerResult(segmentation_masks=[img], pose_landmarks=[], pose_world_landmarks=[])
+        a._process_image(result, img, -1)
